305885d3783126e8fafee7fbe06409213f9ff8aa
crowjambo <evaldascrow@gmail.com>
2019-02-05 00:24:40 +0200

    Add project files.

---
+++ SchoolworkRepo1/Source.c
@@ -0,0 +1,45 @@
+/*
+ARMSTRONG ALGORHITHM implementation in C
+EVALDAS PAULAUAS PI18E
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main() {
+
+	int number, tempNumber, remainder, result = 0, counter = 0;
+
+	printf("Enter an integer: ");
+	scanf_s("%d", &number);
+
+	tempNumber = number;
+
+	while (tempNumber != 0)
+	{
+		tempNumber /= 10;
+		++counter;
+	}
+
+	tempNumber = number;
+
+	while (tempNumber != 0)
+	{
+		remainder = tempNumber % 10;
+		printf("\n remainder %d", remainder);
+		result += pow(remainder, counter);
+		printf("\n result %d", result);
+		tempNumber /= 10;
+		printf("\n temp number %d \n", tempNumber);
+	}
+
+	if (result == number)
+		printf("%d is an Armstrong number. \n", number);
+	else
+		printf("%d is not an Armstrong number. \n", number);
+
+	system("pause");
+
+	return 0;
+}
