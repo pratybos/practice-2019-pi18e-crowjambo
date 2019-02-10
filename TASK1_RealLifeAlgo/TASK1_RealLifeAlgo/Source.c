9d2d3acc7d445c5152a414ecb07f70620e824b52
crowjambo <evaldascrow@gmail.com>
2019-02-10 15:02:21 +0200

    Clearer goals defined
    going to use normal C afterall. Utilize arrays/windows.h/conio.h

--- TASK1_RealLifeAlgo/TASK1_RealLifeAlgo/Source.c
+++ TASK1_RealLifeAlgo/TASK1_RealLifeAlgo/Source.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
+#include <Windows.h>
 
 /*
 
 Real life task, made into algorhithm. THINK LIKE A COMPUTER, divide it into tasks and execute
 
-Switch to using C++? to practice OOP etc.
+Use normal C and just arrays to draw things, maybe WinAPI and extra thread for drawing ( or so ) and regex for clearscreen/console(veryfast)
 
 - Going to store (with a map from arrays)
 - Buying groceries (navigate aswell?), can pick whatever you want
@@ -14,10 +16,28 @@ Switch to using C++? to practice OOP etc.
 
 */
 
-int main() {
 
-	printf("Hello world");
+int main()
+{
+	int key = 0; // getch returns int
+	while (1) {
+		if (_kbhit()) {
+			key = _getch();
+			printf("\t %d \n", key);
 
-	system("pause");
+			if (key == 'w') {			// now it works, remember difference in ASCII value between capital letters and not
+				printf("w has been pressed");
+				break;
+			}
+			else {
+				printf("\n other value %d", key);
+			}
+		}
+		Sleep(200); // needed windows.h inclusion to work
+		printf(" === NOTHING === \n");
+	}
+	
+	printf("\n\n");
+	system("pause"); // can be used for any command like dir or cls (clearscreen) make use of it for prototype
 	return 0;
 }
