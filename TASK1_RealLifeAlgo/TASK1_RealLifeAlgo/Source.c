8d369cb867f3de6966cf3a3c7d84e9d328494fa3
crowjambo <evaldascrow@gmail.com>
2019-02-10 14:06:21 +0200

    reOrganization

---
+++ TASK1_RealLifeAlgo/TASK1_RealLifeAlgo/Source.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+
+Real life task, made into algorhithm. THINK LIKE A COMPUTER, divide it into tasks and execute
+
+
+*/
+
+int main() {
+
+	printf("Hello world");
+
+	system("pause");
+	return 0;
+}
