22e414ce50bbe6e6f1357a81530de2e98d6aa346
crowjambo <evaldascrow@gmail.com>
2019-02-10 14:13:42 +0200

    plans
    extra ideas for project

--- TASK1_RealLifeAlgo/TASK1_RealLifeAlgo/Source.c
+++ TASK1_RealLifeAlgo/TASK1_RealLifeAlgo/Source.c
@@ -5,6 +5,12 @@
 
 Real life task, made into algorhithm. THINK LIKE A COMPUTER, divide it into tasks and execute
 
+Switch to using C++? to practice OOP etc.
+
+- Going to store (with a map from arrays)
+- Buying groceries (navigate aswell?), can pick whatever you want
+- Paying
+- Going home
 
 */
 
