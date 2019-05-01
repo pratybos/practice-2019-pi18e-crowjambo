06a1b1b38bcf8c8f09758bc82f5f3489b38ca7e4
crowjambo <evaldascrow@gmail.com>
2019-05-02 01:42:22 +0300

    all the rest functions in comment form

--- Quiz_Practical_Work/Quiz_Practical_Work/main.c
+++ Quiz_Practical_Work/Quiz_Practical_Work/main.c
@@ -3,14 +3,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-/*
-Given two arrays:
-	int array1[5] = {1, 4, 5, 2, 8};
-	int array2[5] = {9, 1, 3, 2, 3};
-write function to merge array1 -> 14528 and array2 -> 91323 into single digit.
-multiply both numbers and output to console.
-*/
-
 void MergeArraysAndMultiply(int arr1[], int arr2[], int length1, int length2) {
 	char temp[30] = { NULL };
 	char combined1[30] = { NULL };
@@ -33,6 +25,98 @@ void MergeArraysAndMultiply(int arr1[], int arr2[], int length1, int length2) {
 	
 }
 
+
+
+/*
+
+1
+
+void longOutputFunction(){
+  long number = 0b10101L;
+  printf("%li", number);
+}
+
+====================================================================================================
+2
+
+void ArrayPrintOutFromTheBack(int x, int arr[], int arrSize){
+	int i;
+	for(i=0; i<x ; i++ ){
+		printf("\t %d", arr[arrSize-i]);
+	}
+}
+
+int main(void) {
+  int arrayTest[6]= {98, 78, 55, 22, 44, 66};
+  ArrayPrintOutFromTheBack(2, arrayTest,6);
+
+  return 0;
+}
+
+====================================================================================================
+
+3
+====================================================================================================
+
+4
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int NumberGenerator(){
+  int number, choice;
+  while(1){
+	number = rand()%100;
+	printf("Number generated: %d \n", number);
+	printf("Any key to continue. N or n to stop\n");
+
+	choice = getchar();
+	if(choice == 'n' || choice == 'N') return number;
+  }
+}
+
+int main(void) {
+  srand(time(0));
+
+  int randomNumber = NumberGenerator();
+  printf("final number is %d", randomNumber);
+
+
+  return 0;
+}
+
+====================================================================================================
+
+5
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void MergeArraysAndMultiply(int arr1[], int arr2[], int length1, int length2) {
+	char temp[30] = { NULL };
+	char combined1[30] = { NULL };
+	char combined2[30] = { NULL };
+	int i, num1,num2;
+
+	for (i = 0; i < length1; i++) {
+		sprintf(temp, "%d", arr1[i]);
+		strcat(combined1, temp);
+	}
+	num1 = atoi(combined1);
+
+	for (i = 0; i < length2; i++) {
+		sprintf(temp, "%d", arr2[i]);
+		strcat(combined2, temp);
+	}
+	num2 = atoi(combined2);
+
+	printf("final outcome : %d", num1*num2);
+
+}
+
 int main(void) {
 	int array1[5] = { 1, 4, 5, 2, 8 };
 	int array2[5] = { 9, 1, 3, 2, 3 };
@@ -45,3 +129,37 @@ int main(void) {
 	system("pause");
 	return 0;
 }
+
+
+====================================================================================================
+
+6
+
+yes its correct
+
+====================================================================================================
+
+7
+
+all true
+
+====================================================================================================
+
+8
+
+1. c = 65 due too ++c call, in %c (char form) = A ; 2. c = 66 in %c form = B 3. c=67 = C ; 4. c=68 = D ; 5. c=69 = E
+
+====================================================================================================
+
+9
+
+
+====================================================================================================
+
+
+10
+
+====================================================================================================
+
+
+*/
