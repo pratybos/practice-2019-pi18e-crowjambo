a480e206c76f629b89168edfddeffbab0a88f9d6
crowjambo <evaldascrow@gmail.com>
2019-05-04 00:39:53 +0300

    Prototype gui button

--- PRACTICAL_WORK/PRACTICAL_WORK/main.c
+++ PRACTICAL_WORK/PRACTICAL_WORK/main.c
@@ -66,11 +66,18 @@ int main() {
 			al_start_timer(timer);
 
 		#pragma endregion
+		#pragma region Colors
+
+			ALLEGRO_COLOR color1 = al_map_rgb(255, 255, 255);
+
+		#pragma endregion
+
 
 	#pragma endregion
 	#pragma region Installs
 
 			al_install_keyboard();
+			al_install_mouse();
 
 	#pragma endregion
 	#pragma region Event sources and Queues
@@ -79,8 +86,18 @@ int main() {
 			al_register_event_source(event_queue, al_get_keyboard_event_source());
 			al_register_event_source(event_queue, al_get_display_event_source(display));
 			al_register_event_source(event_queue, al_get_timer_event_source(timer));
+			al_register_event_source(event_queue, al_get_mouse_event_source());
 
 	#pragma endregion
+	#pragma region Test Variables
+
+			//mouse position
+			int x = 0;
+			int y = 0;
+			//button press
+			int number = 1;
+	#pragma endregion
+
 
 	//main loop
 	while (!done) {
@@ -89,18 +106,35 @@ int main() {
 		if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
 			done = true;
 		}
-
-
 		else if (event.type == ALLEGRO_EVENT_TIMER) {
 			redraw = true;
 
+		}
+		else if (event.type == ALLEGRO_EVENT_MOUSE_AXES) {
+			x = event.mouse.x;
+			y = event.mouse.y;
+		}
+		else if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
+			// 1 is left click, 2 is right
+			if (event.mouse.button & 1) {
+				//detecting whether test button is clicked or not
+				if (SCREEN_WIDTH / 2 <= x &&
+					x < SCREEN_WIDTH / 2 +100 &&
+					SCREEN_HEIGHT /2 <= y &&
+					y < SCREEN_HEIGHT /2 +50) {
+					number += 1;
+				}
+			}
 		}
 	
 
 
 		if (redraw && al_is_event_queue_empty(event_queue)) {
-
+			//display mouse position for debugging
+			al_draw_textf(font16, color1, SCREEN_WIDTH, SCREEN_HEIGHT-16, ALLEGRO_ALIGN_RIGHT, "x = %d ; y = %d", x, y);
 			
+			al_draw_rectangle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, SCREEN_WIDTH / 2 + 100, SCREEN_HEIGHT / 2 + 50, color1, 1);
+			al_draw_textf(font12, color1, SCREEN_WIDTH / 2+50, SCREEN_HEIGHT / 2 +17, ALLEGRO_ALIGN_CENTER, "%d", number);
 		
 			al_flip_display();
 			al_clear_to_color(al_map_rgb(0, 0, 0));
