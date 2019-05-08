fe1f7913183250811dd9d7dcbeebb2664a7ca49a
crowjambo <evaldascrow@gmail.com>
2019-05-08 13:52:15 +0300

    function pointer array and button value arrays for better button management

--- PRACTICAL_WORK/PRACTICAL_WORK/main.c
+++ PRACTICAL_WORK/PRACTICAL_WORK/main.c
@@ -14,22 +14,10 @@
 #define SCREEN_WIDTH 1024
 #define SCREEN_HEIGHT 768
 
-//global variables
-bool redraw = false;
+//variables
 bool fullyDone = false;
 GameManager mng;
 int FPS = 60;
-
-
-#pragma region ButtonFunctions
-
-//general
-void QuitGame() {
-	fullyDone = true;
-}
-
-
-	#pragma endregion
 
 #pragma endregion
 #pragma region ALLEGRO VARIABLES
@@ -47,12 +35,28 @@ ALLEGRO_TIMER *timer;
 //event queues
 ALLEGRO_EVENT_QUEUE *event_queue;
 #pragma endregion
+#pragma region FUNCTIONS
+
+int QuitGame() {
+	return 1;
+}
+
+int TestFun() {
+	return 1;
+}
+
+int TestFun2() {
+	return 1;
+};
+
+int(*buttonFuncArray[3]) = { TestFun, TestFun2, QuitGame};
+
+#pragma endregion
 #pragma region SCENES
 
 int Start_Scene() {
 
-#pragma region Custom Variables
-
+#pragma region Variables
 	int i = 0;
 	//mouse position
 	int x = 0;
@@ -61,16 +65,36 @@ int Start_Scene() {
 	bool leftClick = false;
 	//function main loop end condition
 	bool done = false;
+	bool redraw = false;
+	//menu button detection variable
+	int buttonVal[3] = { 0,0,0 };
+
+	#pragma region Buttons
+
+	Button buttonsMain[3];
+	for (i = 0; i < 3; i++) {
+		buttonsMain[i] = buttonInit(SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT / 3+i*100, 200, 50, colors[0], colors[1], colors[2], colors[3], buttonFuncArray[i], "Empty");
+	}
+	strcpy_s(buttonsMain[0].label, 20, "Start Game");
+
+	strcpy_s(buttonsMain[1].label, 20, "Rankings");
+
+	strcpy_s(buttonsMain[2].label, 20, "Quit Game");
 
 
-#pragma endregion
+	#pragma endregion
 
-	done = false;
+
+	#pragma endregion
+
 	//main loop
 	while (!done) {
+		//reset button press values
+		for (i = 0; i < 3; i++) {
+			buttonVal[i] = 0;
+		}
 
-
-#pragma region Events
+	#pragma region Events
 
 		ALLEGRO_EVENT event;
 		al_wait_for_event(event_queue, &event);
@@ -93,8 +117,6 @@ int Start_Scene() {
 			// 1 is left click, 2 is right
 			if (event.mouse.button & 1) {
 				leftClick = true;
-				mng.currentScene = MAIN;
-				done = true;
 
 			}
 		}
@@ -105,109 +127,58 @@ int Start_Scene() {
 			}
 		}
 
-#pragma endregion
-#pragma region Drawing
+	#pragma endregion
+	#pragma region Drawing
 
 		if (redraw && al_is_event_queue_empty(event_queue)) {
 			//display mouse position for debugging
 			al_draw_textf(font16, colors[0], SCREEN_WIDTH, SCREEN_HEIGHT - 16, ALLEGRO_ALIGN_RIGHT, "x = %d ; y = %d", x, y);
 
-			al_draw_text(font16, colors[0], SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, ALLEGRO_ALIGN_RIGHT, "this is start scene");
+			//button checks		
+			for (i = 0; i < 3; i++) {
+				buttonVal[i] = checkButton(&buttonsMain[i], x, y, leftClick, buttonVal[i]);
+			}
 
-			al_flip_display();
-			al_clear_to_color(al_map_rgb(0, 0, 0));
-		}
-
-#pragma endregion
-
-	}
-#pragma region Clean up / destroy
-
-	//destroy everything local here
-
-#pragma endregion
-	sceneLoad();
-	return 0;
-}
-int Other_Scene() {
-#pragma region Custom Variables
-
-	int i = 0;
-	//mouse position
-	int x = 0;
-	int y = 0;
-	//mouse left click hold detection
-	bool leftClick = false;
-	//function main loop end condition
-	bool done = false;
-
-
-#pragma endregion
-
-	done = false;
-	//main loop
-	while (!done) {
-
-
-#pragma region Events
-
-		ALLEGRO_EVENT event;
-		al_wait_for_event(event_queue, &event);
-		if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-			fullyDone = true;
-			done = true;
-		}
-		else if (event.type == ALLEGRO_EVENT_TIMER) {
-			redraw = true;
-
-			//could add+1 to some variable here and have an IF statement at certain value to create an even that triggers every X amount of frames, rather than using seperate timer ( for example a slower animation than 60fps etc.)
-
-		}
-		else if (event.type == ALLEGRO_EVENT_MOUSE_AXES) {
-			x = event.mouse.x;
-			y = event.mouse.y;
-
-		}
-		else if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN) {
-			// 1 is left click, 2 is right
-			if (event.mouse.button & 1) {
-				leftClick = true;
-				mng.currentScene = START;
+			//button drawing
+			for (i = 0; i < 3; i++) {
+				drawButton(buttonsMain[i], font16, 16);
+			}
+				
+			//button results check
+			if (buttonVal[0] == 1) {
+				printf("start was pressed");
+			}
+			else if (buttonVal[1] == 1) {
+				printf("ranking was pressed");
+			}
+			else if (buttonVal[2] == 1) {
+				fullyDone = true;
 				done = true;
-
-			}
-		}
-		else if (event.type == ALLEGRO_EVENT_MOUSE_BUTTON_UP) {
-			if (event.mouse.button & 1) {
-				leftClick = false;
-
 			}
 		}
 
-#pragma endregion
-#pragma region Drawing
 
-		if (redraw && al_is_event_queue_empty(event_queue)) {
-			//display mouse position for debugging
-			al_draw_textf(font16, colors[0], SCREEN_WIDTH, SCREEN_HEIGHT - 16, ALLEGRO_ALIGN_RIGHT, "x = %d ; y = %d", x, y);
-
-			al_draw_text(font16, colors[0], SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, ALLEGRO_ALIGN_RIGHT, "this is main scene");
 
 			al_flip_display();
 			al_clear_to_color(al_map_rgb(0, 0, 0));
 		}
 
-#pragma endregion
+	#pragma endregion
 
-	}
-#pragma region Clean up / destroy
+
+	#pragma region Clean up / destroy
 
 	//destroy everything local here
 
-#pragma endregion
+	#pragma endregion
 	sceneLoad();
 	return 0;
 }
+int Main_Scene(){}
+int Home_Scene(){}
+int CarShop_Scene(){}
+int PartsShop_Scene(){}
+int Race_Scene(){}
 
 #pragma endregion
 
@@ -225,7 +196,7 @@ int sceneLoad() {
 		Start_Scene();
 		break;
 	case MAIN:
-		Other_Scene();
+
 		break;
 	case HOME:
 
