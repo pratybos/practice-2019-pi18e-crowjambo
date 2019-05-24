a0bb06aa00e2340d8bd2373c59a1d2d6340e6f55
crowjambo <evaldascrow@gmail.com>
2019-05-24 13:54:18 +0300

    hashtable adt inits

--- TASK4/TASK4/main.c
+++ TASK4/TASK4/main.c
@@ -3,12 +3,15 @@
 TASK 4 - EVALDAS PAULAUSKAS PI18E
 
 */
-
+#include "HashTable.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 
 int main() {
 
-
+	ht_hash_table* ht = ht_new();
+	ht_del_hash_table(ht);
 
 	system("pause");
 	return 0;
