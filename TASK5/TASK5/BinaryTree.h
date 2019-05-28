039be595e8a928f7f822e19d0a98a722e9b039b2
crowjambo <evaldascrow@gmail.com>
2019-05-28 20:44:51 +0300

    tree adt addition

---
+++ TASK5/TASK5/BinaryTree.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "UtilityLibs.h"
+
+#ifndef BINARYTREE_H
+#define BINARYTREE_H
+
+enum DisplayMode_Tree {
+	InOrder = 1,
+	ReverseOrder = 2,
+	RootFirst = 3,
+	OnlyRoot = 4
+};
+
+struct tree_node {
+	int data;
+	struct tree_node *left_child;
+	struct tree_node *right_child;
+};
+typedef struct tree_node tree_node;
+
+struct tree {
+	int size;
+	tree_node *root;
+};
+typedef struct tree tree;
+
+
+// FUNCTIONS
+tree *create_tree();
+tree_node *createTreeNode(int data);
+bool is_tree_empty(tree *t);
+void insert_to_tree(tree *t, int data);
+void display_tree(tree_node *tempRoot, enum PrintMode_Tree x);
+void destroy_tree(tree_node *tempRoot);
+void insert_left(tree_node *n, int data);
+void insert_right(tree_node *n, int data);
+void set_root(tree *Tree, int data);
+int get_root(tree *Tree);
+int get_left_child(tree_node *n);
+int get_right_child(tree_node *n);
+
+#endif
---
+++ TASK5/TASK5/main.c
@@ -0,0 +1,21 @@
+#include "UtilityLibs.h"
+#include "BinaryTree.h"
+
+//Create a console program that can guess your invented word for a freely chosen topic
+
+
+int main() {
+
+	//INIT, note -  root is NULL 
+	tree *someTree = create_tree();
+	
+	/*
+	TODO:
+		- Multiple data structures is required
+		- 
+	
+	*/
+
+	system("pause");
+	return 0;
+}
