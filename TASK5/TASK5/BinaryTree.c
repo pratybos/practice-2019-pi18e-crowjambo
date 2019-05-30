2ee447f73005692153905efad43262602a67242f
crowjambo <evaldascrow@gmail.com>
2019-05-30 23:46:32 +0300

    updated binary tree for different data

--- TASK5/TASK5/BinaryTree.c
+++ TASK5/TASK5/BinaryTree.c
@@ -8,7 +8,7 @@ tree *create_tree() {
 	return t;
 }
 
-tree_node *createTreeNode(int data) {
+tree_node *createTreeNode(data data) {
 	tree_node *temp = (tree_node*)malloc(sizeof(tree_node));
 	temp->left_child = temp->right_child = NULL;
 	temp->data = data;
@@ -19,53 +19,111 @@ bool is_tree_empty(tree *t) {
 	return t->size == 0;
 }
 
-// Insert new values all ordered by value size automatically
-void insert_to_tree(tree *t, int data) {
-	tree_node *temp = createTreeNode(data);
-	// if empty, just become the root
-	if (is_tree_empty(t)) {
-		t->root = temp;
-		t->size++;
+
+
+// recursive traversal, but now freeing up nodes
+void destroy_tree(tree_node *tempRoot) {
+	if (tempRoot == NULL) {
 		return;
 	}
-
-	// used for traversing the tree
-	tree_node *temp2 = t->root;
-
-	while (1) {
-		// if new data is smaller than roots left child
-		if (data < temp2->data) {
-			// if roots left child has space take it
-			if (temp2->left_child == NULL) {
-				temp2->left_child = temp;
-				t->size++;
-				return;
-			}
-			// no space - go down the left tree lower
-			else {
-				temp2 = temp2->left_child;
-			}
-		}
-		// new data is bigger than roots data - take right side
-		else if (temp2->data < data) {
-			// if theres space, take it immediately
-			if (temp2->right_child == NULL) {
-				temp2->right_child = temp;
-				t->size++;
-				return;
-			}
-			// no space = traverse further down
-			else {
-				temp2 = temp2->right_child;
-			}
-
-		}
-		// EQUAL, no reason to add equal value
-		else {
-			break;
-		}
-	}
+	destroy_tree(tempRoot->left_child);
+	destroy_tree(tempRoot->right_child);
+	free(tempRoot);
 }
+
+// insert value to left, if left child already exist, continue down the tree until left child is empty
+void insert_left(tree_node *n, data data) {
+	if (n->left_child == NULL) {
+		n->left_child = createTreeNode(data);
+		return;
+	}
+	insert_left(n->left_child, data);
+}
+
+//traverse until empty spot and add to the right
+void insert_right(tree_node *n, data data) {
+	if (n->right_child == NULL) {
+		n->right_child = createTreeNode(data);
+		return;
+	}
+	insert_left(n->right_child, data);
+}
+
+// insert new root, if root exist, replace it
+void set_root(tree *Tree, data data) {
+	if (Tree->root == NULL) {
+		Tree->root = createTreeNode(data);
+		return;
+	}
+	tree_node *temp = Tree->root;
+	Tree->root = createTreeNode(data);
+	Tree->root->left_child = temp->left_child;
+	Tree->root->right_child = temp->right_child;
+	free(temp);
+}
+
+data get_root(tree *Tree) {
+	return Tree->root->data;
+}
+
+data get_left_child(tree_node *n) {
+	return n->left_child->data;
+}
+
+data get_right_child(tree_node *n) {
+	return n->right_child->data;
+}
+
+#pragma region Wont work with new DATA struct
+
+
+// Insert new values all ordered by value size automatically
+//void insert_to_tree(tree *t, data data) {
+//	tree_node *temp = createTreeNode(data);
+//	// if empty, just become the root
+//	if (is_tree_empty(t)) {
+//		t->root = temp;
+//		t->size++;
+//		return;
+//	}
+//
+//	// used for traversing the tree
+//	tree_node *temp2 = t->root;
+//
+//	while (1) {
+//		// if new data is smaller than roots left child
+//		if (data < temp2->data) {
+//			// if roots left child has space take it
+//			if (temp2->left_child == NULL) {
+//				temp2->left_child = temp;
+//				t->size++;
+//				return;
+//			}
+//			// no space - go down the left tree lower
+//			else {
+//				temp2 = temp2->left_child;
+//			}
+//		}
+//		// new data is bigger than roots data - take right side
+//		else if (temp2->data < data) {
+//			// if theres space, take it immediately
+//			if (temp2->right_child == NULL) {
+//				temp2->right_child = temp;
+//				t->size++;
+//				return;
+//			}
+//			// no space = traverse further down
+//			else {
+//				temp2 = temp2->right_child;
+//			}
+//
+//		}
+//		// EQUAL, no reason to add equal value
+//		else {
+//			break;
+//		}
+//	}
+//}
 
 // Pay attention that you have to send in tree->root !!! else it works,but crashes
 // 1 ordered from small. 2 big to small. 3 root first then rest. 4 only root
@@ -107,56 +165,4 @@ void display_tree(tree_node *tempRoot, enum PrintMode_Tree x) {
 		return;
 	}
 }
-
-// recursive traversal, but now freeing up nodes
-void destroy_tree(tree_node *tempRoot) {
-	if (tempRoot == NULL) {
-		return;
-	}
-	destroy_tree(tempRoot->left_child);
-	destroy_tree(tempRoot->right_child);
-	free(tempRoot);
-}
-
-// insert value to left, if left child already exist, continue down the tree until left child is empty
-void insert_left(tree_node *n, int data) {
-	if (n->left_child == NULL) {
-		n->left_child = createTreeNode(data);
-		return;
-	}
-	insert_left(n->left_child, data);
-}
-
-//traverse until empty spot and add to the right
-void insert_right(tree_node *n, int data) {
-	if (n->right_child == NULL) {
-		n->right_child = createTreeNode(data);
-		return;
-	}
-	insert_left(n->right_child, data);
-}
-
-// insert new root, if root exist, replace it
-void set_root(tree *Tree, int data) {
-	if (Tree->root == NULL) {
-		Tree->root = createTreeNode(data);
-		return;
-	}
-	tree_node *temp = Tree->root;
-	Tree->root = createTreeNode(data);
-	Tree->root->left_child = temp->left_child;
-	Tree->root->right_child = temp->right_child;
-	free(temp);
-}
-
-int get_root(tree *Tree) {
-	return Tree->root->data;
-}
-
-int get_left_child(tree_node *n) {
-	return n->left_child->data;
-}
-
-int get_right_child(tree_node *n) {
-	return n->right_child->data;
-}
+#pragma endregion
--- TASK5/TASK5/BinaryTree.h
+++ TASK5/TASK5/BinaryTree.h
@@ -4,6 +4,11 @@
 #ifndef BINARYTREE_H
 #define BINARYTREE_H
 
+typedef struct {
+	char question[100];
+	char answer[20];
+}data;
+
 enum DisplayMode_Tree {
 	InOrder = 1,
 	ReverseOrder = 2,
@@ -12,7 +17,7 @@ enum DisplayMode_Tree {
 };
 
 struct tree_node {
-	int data;
+	data data;
 	struct tree_node *left_child;
 	struct tree_node *right_child;
 };
@@ -27,16 +32,14 @@ typedef struct tree tree;
 
 // FUNCTIONS
 tree *create_tree();
-tree_node *createTreeNode(int data);
+tree_node *createTreeNode(data data);
 bool is_tree_empty(tree *t);
-void insert_to_tree(tree *t, int data);
-void display_tree(tree_node *tempRoot, enum PrintMode_Tree x);
 void destroy_tree(tree_node *tempRoot);
-void insert_left(tree_node *n, int data);
-void insert_right(tree_node *n, int data);
-void set_root(tree *Tree, int data);
-int get_root(tree *Tree);
-int get_left_child(tree_node *n);
-int get_right_child(tree_node *n);
+void insert_left(tree_node *n, data data);
+void insert_right(tree_node *n, data data);
+void set_root(tree *Tree, data data);
+data get_root(tree *Tree);
+data get_left_child(tree_node *n);
+data get_right_child(tree_node *n);
 
 #endif
