c939e10756310bb59bc0c10094fc51e28629f831
crowjambo <evaldascrow@gmail.com>
2019-06-02 23:07:21 +0300

    new writing method using input data

--- TASK5/TASK5/main.c
+++ TASK5/TASK5/main.c
@@ -104,11 +104,6 @@ int main() {
 		set_root(someTree, "First Question", "None");
 	}
 
-	
-
-
-
-
 #pragma endregion
 
 
@@ -118,6 +113,9 @@ int main() {
 
 	//navigator node init
 	tree_node *currentNode = someTree->root;
+
+	//modifier var
+	bool isModified = false;
 
 	//main loop
 	while (!finished) {
@@ -151,6 +149,7 @@ int main() {
 							insert_left(currentNode, question, "None");
 							someTree->size += 1;
 							anotherInput = true;
+							isModified = true;
 						}
 						else if (input == '2') {
 							printf("Enter answer: ");
@@ -159,6 +158,7 @@ int main() {
 							insert_left(currentNode, "No Question", answer);
 							someTree->size += 1;
 							anotherInput = true;
+							isModified = true;
 						}
 					}
 
@@ -182,6 +182,7 @@ int main() {
 							insert_right(currentNode, question, "None");
 							someTree->size += 1;
 							anotherInput = true;
+							isModified = true;
 						}
 						else if (input == '2') {
 							printf("Enter answer: ");
@@ -190,6 +191,7 @@ int main() {
 							insert_right(currentNode, "No Question", answer);
 							someTree->size += 1;
 							anotherInput = true;
+							isModified = true;
 						}
 					}
 				}
@@ -200,49 +202,104 @@ int main() {
 		}
 	}
 
-	//save created nodes to file
-	outfile = fopen("data.dat", "w");
-	//temp list creation to store binary tree data
-	list someList;
-	someList.head = NULL;
-	someList.size = 0;
-	
-	//save tree values to list
-	tree_values(&someList, someTree->root);
-	
+	if (isModified) {
+		//save created nodes to file
+		outfile = fopen("data.dat", "w");
+		//temp list creation to store binary tree data
+		list someList;
+		someList.head = NULL;
+		someList.size = 0;
 
-	//save list values to file
-	testStruct temp;
-	//traversal node
-	list_node *temp_node = someList.head;
-	while (temp_node != NULL) {
-		//copy data to temp testStruct
-		strcpy(temp.question, temp_node->testData.question);
-		strcpy(temp.answer, temp_node->testData.answer);
-		//write data to file
-		fwrite(&temp, sizeof(testStruct), 1, outfile);
-		//traverse to next list node
-		temp_node = temp_node->next;
+		//save tree values to list
+		//tree_values(&someList, someTree->root);
+
+#pragma region NewWriting Method
+		//Write the data using replay information, this way everything is CLEAN
+		//gotta append values in order to linkedList
+
+		//VARS
+		char someChar2 = 'y';
+		int otherCounter = 0;
+		tree_node *treeTrav = someTree->root;
+		list_node *listTrav = someList.head;
+
+		testStruct testingStruct2;
+		strcpy(testingStruct2.question, someTree->root->data.question);
+		strcpy(testingStruct2.answer, someTree->root->data.answer);
+		//send in the root to list
+		list_new_insert(&someList, testingStruct2);
+		//travel next
+		listTrav = listTrav->next;
+
+		//while theres user input data in array
+		while (someChar2 == 'y' || someChar2 == 'n' || someChar2 == 'e') {
+			//if end of session symbol
+			if (userInput.input[otherCounter] == 'e') {
+				//reset binary tree traversal to beginning
+				treeTrav = someTree->root;
+			}
+
+			else if (userInput.input[otherCounter] == 'y') {
+				//list_new_insert(&someList,)
+
+				insert_left(treeTrav, listTrav->testData.question, listTrav->testData.answer);
+				//move up to the left
+				treeTrav = treeTrav->left_child;
+			}
+			else if (userInput.input[otherCounter] == 'n') {
+				insert_right(treeTrav, listTrav->testData.question, listTrav->testData.answer);
+				//move up to the right
+				treeTrav = treeTrav->right_child;
+			}
+
+			//read next stored input
+			otherCounter++;
+			someChar2 = userInput.input[otherCounter];
+			//next list value
+			if (listTrav->next != NULL) {
+				listTrav = listTrav->next;
+			}
+		}
+		//inputCounter = otherCounter;
+
+
+#pragma endregion
+
+
+
+		//save list values to file
+		testStruct temp;
+		//traversal node
+		list_node *temp_node = someList.head;
+		while (temp_node != NULL) {
+			//copy data to temp testStruct
+			strcpy(temp.question, temp_node->testData.question);
+			strcpy(temp.answer, temp_node->testData.answer);
+			//write data to file
+			fwrite(&temp, sizeof(testStruct), 1, outfile);
+			//traverse to next list node
+			temp_node = temp_node->next;
+		}
+		//close file
+		fclose(outfile);
+
+		//to the end of current replay data inputs save a special symbol to indicate end of that session
+		//length of current input array
+		char lengthX = 'y';
+		i = 0;
+		while (lengthX == 'y' || lengthX == 'n' || lengthX == 'e') {
+			lengthX = userInput.input[i];
+			i++;
+		}
+		//add to the last element e to indicate end
+		userInput.input[i - 1] = 'e';
+
+		//save inputs
+		outfile = fopen("input.dat", "w");
+		fwrite(&userInput, sizeof(InputStruct), 1, outfile);
+		fclose(outfile);
 	}
-	//close file
-	fclose(outfile);
-
-	//to the end of current replay data inputs save a special symbol to indicate end of that session
-	//length of current input array
-	char lengthX = 'y';
-	i = 0;
-	while (lengthX == 'y' || lengthX == 'n' || lengthX == 'e') {
-		lengthX = userInput.input[i];
-		i++;
-	}
-	//add to the last element e to indicate end
-	userInput.input[i-1] = 'e';
-
-	//save inputs
-	outfile = fopen("input.dat", "w");
-	fwrite(&userInput,sizeof(InputStruct),1,outfile);
-	fclose(outfile);
-
+	
 
 	system("pause");
 	return 0;
